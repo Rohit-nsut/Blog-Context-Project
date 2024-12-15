4f67649080bbae9f57fbca077811eddfb9545e1f
Roht <rohittondak38@gmail.com>
2024-12-16 02:13:24 +0530

    New Commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div>
-        <header className='w-full border-b-2 border-gray-300 shadow-md  fixed bg-white top-0'>
+        <header className='w-full border-b-2 border-pink-500 shadow-md  fixed bg-blue-500 top-0'>
           <h1 className=' uppercase text-3xl font-bold text-center my-4 mx-auto '>codehelp blogs</h1>
         </header>
     </div>
--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -7,7 +7,7 @@ const Pagination = () => {
     const {page, totalPages, handlePageChange} = useContext(AppContext);
  
     return (
-        <div className="border-t-2 border-gray-300 fixed bottom-0 w-full bg-white">
+        <div className="border-t-2 border-pink-500 fixed bottom-0 w-full bg-blue-500">
 
             <div className="w-11/12 md:w-9/12 lg:w-[45%] flex justify-between mx-auto my-2">
 
